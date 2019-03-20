c4371c3848cb80cdfa548045a43bbc7cb3c69118
LEGRANDZOUG <alexandre.melix@gmail.com>
2019-03-20 14:44:03 +0100

    t

---
+++ index.php
@@ -0,0 +1,217 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <meta name="theme-color" content="#000000" />
+  <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
+  <title>ESD-games studio</title>
+  <!--Bootstrap-->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <!--Fontawesome-->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+    integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+  <!--Google font-->
+  <link href="https://fonts.googleapis.com/css?family=Roboto:700" rel="stylesheet">
+  <!--Css-->
+  <link rel="stylesheet" href="app/dist/assets/css/style.css">
+</head>
+
+<body>
+  <!--Navbar-->
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item dropdown">
+          <div class="d-flex justify-content-between">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+              aria-haspopup="true" aria-expanded="false">
+              <i class="fas fa-bars"></i>
+              <!--Supprimer la fleche avec css-->
+            </a>
+            <img src="app/src/img/logo_esd.ext__1.png" alt="">
+            <button><i class="fas fa-medal"></i></button>
+
+
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="#">Action</a>
+              <a class="dropdown-item" href="#">Another action</a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
+    </div>
+  </nav>
+  <div class="bd-example">
+    <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+      <ol class="carousel-indicators">
+        <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+        <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+        <li data-target="#carouselExampleCaptions" data-slide-to="3"></li>
+        <li data-target="#carouselExampleCaptions" data-slide-to="4"></li>
+        <li data-target="#carouselExampleCaptions" data-slide-to="5"></li>
+        <li data-target="#carouselExampleCaptions" data-slide-to="6"></li>
+        <li data-target="#carouselExampleCaptions" data-slide-to="7"></li>
+        <li data-target="#carouselExampleCaptions" data-slide-to="8"></li>
+        <li data-target="#carouselExampleCaptions" data-slide-to="9"></li>
+        <li data-target="#carouselExampleCaptions" data-slide-to="10"></li>
+      </ol>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="app/src/img/background.gif" class="d-block w-100" alt="...">
+          <div class="carousel-caption d-none d-md-block">
+              <button type="button" class="btn btn-primary">Primary</button>
+            <h2>Tempest Game</h2>
+            <p>by DERIT Nicolas</p>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img src="app/src/img/background.gif" class="d-block w-100" alt="...">
+          <div class="carousel-caption d-none d-md-block">
+              <button type="button" class="btn btn-primary">Primary</button>
+            <h2>Stargazing</h2>
+            <p>by APPERCE Arthur</p>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img src="app/src/img/background.gif" class="d-block w-100" alt="...">
+          <div class="carousel-caption d-none d-md-block">
+              <button type="button" class="btn btn-primary">Primary</button>
+            <h2>Pain</h2>
+            <p>by BELMOUAZ Aniss</p>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img src="app/src/img/background.gif" class="d-block w-100" alt="...">
+          <div class="carousel-caption d-none d-md-block">
+              <button type="button" class="btn btn-primary">Primary</button>
+            <h2>????????</h2>
+            <p>by Gabriel Benit</p>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img src="app/src/img/background.gif" class="d-block w-100" alt="...">
+          <div class="carousel-caption d-none d-md-block">
+              <button type="button" class="btn btn-primary">Primary</button>
+            <h2>Les fleures du mal</h2>
+            <p>by BERTAUD Thomas</p>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img src="app/src/img/background.gif" class="d-block w-100" alt="...">
+          <div class="carousel-caption d-none d-md-block">
+              <button type="button" class="btn btn-primary">Primary</button>
+            <h2>School Fighter</h2>
+            <p>by BESSONNET Charlélie</p>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img src="app/src/img/background.gif" class="d-block w-100" alt="...">
+          <div class="carousel-caption d-none d-md-block">
+              <button type="button" class="btn btn-primary">Primary</button>
+            <h2>Zombie Rush</h2>
+            <p>by DULHOSTE Roxane</p>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img src="app/src/img/background.gif" class="d-block w-100" alt="...">
+          <div class="carousel-caption d-none d-md-block">
+              <button type="button" class="btn btn-primary">Primary</button>
+            <h2>Macronie The Game </h2>
+            <p>by MUTELET Simon</p>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img src="app/src/img/background.gif" class="d-block w-100" alt="...">
+          <div class="carousel-caption d-none d-md-block">
+              <button type="button" class="btn btn-primary">Primary</button>
+            <h2>L’odysée des planète</h2>
+            <p>by SANCHEZ Clara</p>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img src="app/src/img/background.gif" class="d-block w-100" alt="...">
+          <div class="carousel-caption d-none d-md-block">
+              <button type="button" class="btn btn-primary">Primary</button>
+            <h2>Spellsign</h2>
+            <p>by ????</p>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img src="app/src/img/background.gif" class="d-block w-100" alt="...">
+          <div class="carousel-caption d-none d-md-block">
+              <button type="button" class="btn btn-primary">Primary</button>
+            <h2>The Swapkat</h2>
+            <p>by PILLAUTT Clara</p>
+          </div>
+        </div>
+      </div>
+      <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+  </div>
+
+<!--PHP-->
+<a href="index.php?game=macron">Voter</a>
+<br>
+
+<?php
+$game = "macron";
+// Connect to database
+$db = new PDO('mysql:host=localhost;dbname=retro-game', 'root', 'root');
+
+// Reques to to database (get nb_vote)
+$q = $db->prepare("SELECT * FROM votes WHERE slug_game = :game");
+$q->bindParam(":game", $game);
+$q->execute();
+
+$data = $q->fetch(PDO::FETCH_ASSOC);
+
+echo $data["nb_votes"] . " Votes";
+
+// Reques to to database (get nb_vote)
+$q = $db->prepare("SELECT * FROM votes WHERE slug_game = :game");
+$q->bindParam(":game", $_GET["game"]);
+$q->execute();
+
+$nb_vote = $q->fetch(PDO::FETCH_ASSOC);
+
+$nb_votes = $nb_vote["nb_votes"] +1 ;
+
+$q = $db->prepare("UPDATE votes SET nb_votes= :nb_votes WHERE slug_game = :game");
+$q->bindParam(":nb_votes", $nb_votes);
+$q->bindParam(":game", $_GET["game"]);
+$q->execute();
+
+
+
+?>
+
+
+  <!--JS-->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+    crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+    integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+    crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+    integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+    crossorigin="anonymous"></script>
+</body>
+
+</html>
