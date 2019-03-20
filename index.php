1108d72bea2ad45633d9a510bddc5d8b83011cb0
LEGRANDZOUG <alexandre.melix@gmail.com>
2019-03-20 19:42:19 +0100

    On a trouvé

--- index.php
+++ index.php
@@ -167,6 +167,7 @@
 
 <!--PHP-->
 <a href="index.php?game=macron">Voter</a>
+<a href="index.php?game=tempest">Voter2</a>
 <br>
 
 <?php
@@ -197,7 +198,34 @@ $q->bindParam(":nb_votes", $nb_votes);
 $q->bindParam(":game", $_GET["game"]);
 $q->execute();
 
+// tempest
 
+$game2 = "tempest";
+// Connect to database
+$db = new PDO('mysql:host=localhost;dbname=retro-game', 'root', 'root');
+
+// Reques to to database (get nb_vote)
+$q = $db->prepare("SELECT * FROM votes WHERE slug_game = :game");
+$q->bindParam(":game", $game2);
+$q->execute();
+
+$data = $q->fetch(PDO::FETCH_ASSOC);
+
+echo $data["nb_votes"] . " Votes";
+
+// Reques to to database (get nb_vote)
+$q = $db->prepare("SELECT * FROM votes WHERE slug_game = :game");
+$q->bindParam(":game", $_GET["game"]);
+$q->execute();
+
+$nb_vote = $q->fetch(PDO::FETCH_ASSOC);
+
+$nb_votes = $nb_vote["nb_votes"] +1 ;
+
+$q = $db->prepare("UPDATE votes SET nb_votes= :nb_votes WHERE slug_game = :game");
+$q->bindParam(":nb_votes", $nb_votes);
+$q->bindParam(":game", $_GET["game"]);
+$q->execute();
 
 ?>
 
